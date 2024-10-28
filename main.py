fae02d34425b4a9126ad9dfb03def7793d7ad6ad
JayuWork <jayanand.work@gmail.com>
2024-10-28 12:17:29 +0400

    Basic implementation of RAG

--- main.py
+++ main.py
@@ -1,5 +1,69 @@
 import os
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import WebBaseLoader
+import bs4
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings   
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 load_dotenv()
 
+def getStarted():
+    print("Getting Started")
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+    # 1. Step 1: Load
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+    # Step 2: Split
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    
+    # Step 3: Vectorize
+    vectorstore = InMemoryVectorStore.from_documents(
+        documents=splits, embedding=OpenAIEmbeddings()
+    )
+    retriever = vectorstore.as_retriever()
+
+    # 2. Incorporate the retriever into a question-answering chain.
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    # 3. Test the chain 
+    response = rag_chain.invoke({"input": "What is Task Decomposition?"})
+    print(response["answer"])
+
+
+
+
+
+if __name__ == "__main__":
+    print("Starting")
+    getStarted()
+    print("Finished")
